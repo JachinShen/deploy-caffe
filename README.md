0381010a88ca02b1ba2b0d099b8604e4bfd438fd
jachinshen <jachinshen@gmail.com>
2017-08-09 14:10:11 +0800

    add comments

---
+++ README.md
@@ -0,0 +1,7 @@
+# Deploy-Cpp-Caffe
+
+这是一个通过调用 Caffe 的c++接口来部署 Caffe 模型的示例程序。
+为了免除编译错误的麻烦，可以放在 caffe 目录下的 examples 进行编译。
+
+This is a demo using c++ api of Caffe to deploy model.
+To get rid of troubles about building, you can place it into caffe/examples and then make it.
--- classification.cpp
+++ classification.cpp
@@ -1,7 +1,9 @@
 #include "/home/jachinshen/Apps/caffe/include/caffe/caffe.hpp"
+//replace with you own path
 #include <string>
 #include <vector>
 #include <iostream>
+//opencv
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -9,6 +11,13 @@ using namespace caffe;
 using namespace cv;
 using namespace std;
 
+/********************************************************
+Function name : get_blob_index
+	get the id of the blob from the name of this blob
+	
+	the first argument is the model
+	the second argument is the name of the blob
+********************************************************/
 unsigned int get_blob_index(boost::shared_ptr< Net<float> > & net, char *query_blob_name)
 {
     std::string str_query(query_blob_name);    
@@ -22,10 +31,22 @@ unsigned int get_blob_index(boost::shared_ptr< Net<float> > & net, char *query_b
     }
 }
 
+/********************************************************
+Function name : caffe_forward
+	put the data into the model and calculate the results
+	
+	the first argument is the model
+	the second model is the pointer to the data (must be continuous)
+	the number of the data that the model read is up to the size of the data layer
+*********************************************************/
 void caffe_forward(boost::shared_ptr< Net<float> > & net, float *data_ptr)
 {
+//get the id of data layer from the name"data"
+//you can replace the name with your own data layer or any layer you want to put data
     char query_blob_name[10] = "data"; /* data, conv1, pool1, norm1, fc6, prob, etc */
     unsigned int blob_id = get_blob_index(net, query_blob_name);
+    
+    //or, you can directly give the blob_id, like blob_id=0;(0 is common for data layer) 
 
     cout<<"data blob id:"<<blob_id<<endl;
     Blob<float>* input_blobs = net->input_blobs()[blob_id];
@@ -35,11 +56,12 @@ void caffe_forward(boost::shared_ptr< Net<float> > & net, float *data_ptr)
             memcpy(input_blobs->mutable_cpu_data(), data_ptr,
                     sizeof(float) * input_blobs->count());
             break;
-            /*case Caffe::GPU:
-              cudaMemcpy(input_blobs->mutable_gpu_data(), data_ptr,
-              sizeof(float) * input_blobs->count(), cudaMemcpyHostToDevice);
-              break;\
-              */
+//if you haven't installed CUDA, comment this case
+        case Caffe::GPU:
+            cudaMemcpy(input_blobs->mutable_gpu_data(), data_ptr,
+            sizeof(float) * input_blobs->count(), cudaMemcpyHostToDevice);
+            break;
+//comment to here              
         default: break;
     } 
     //net->ForwardPrefilled();
@@ -53,28 +75,28 @@ int main()
     Phase phase = TEST; /* or TRAIN */
     Caffe::set_mode(Caffe::CPU);
     boost::shared_ptr< Net<float> > net(new caffe::Net<float>(proto, phase));
-    char model[100] = "/home/jachinshen/Apps/caffe/examples/cpp_minst/lenet_iter_10000.caffemodel";    
+    char model[100] = "/home/jachinshen/Apps/caffe/examples/cpp_minst/lenet_iter_10000.caffemodel";//load arguments of model    
     net->CopyTrainedLayersFrom(model);
 
     Mat img_raw = imread("/home/jachinshen/Apps/caffe/examples/cpp_minst/_00175.png", 1); 
     Mat img;
     cvtColor(img_raw, img, CV_BGR2GRAY);
-    Mat data(28, 28, CV_32FC1);
+    //Mat data(28, 28, CV_32FC1);
     resize(img, img, Size(28, 28));
     threshold(img, img, 128, 1, THRESH_BINARY_INV);
     imshow("img", img);
     waitKey(0);
-    cout<< img<<endl;
+    //cout<< img<<endl;
     //normalize(data, data, 0.0, 1.0, NORM_MINMAX);
 
-    float data_ptr[784];
+    float data_ptr[784];//for data layer
     uchar* p_data;
     for( int i=0; i<28; i++)
     {
         p_data = img.ptr< uchar>(i);
         for(int j=0; j<28; j++)
         {
-        	data_ptr[28*i+j]=(float)*(p_data+j);
+        	data_ptr[28*i+j]=(float)*(p_data+j);//get data and convert to float
         }
     }
     //for(int i=0;i<784;i++)
@@ -87,7 +109,7 @@ int main()
     boost::shared_ptr<Blob<float> > blob = net->blobs()[blob_id];
     unsigned int num_data = blob->count(); /* NCHW=10x96x55x55 */
     std::cout<<"data number:"<<num_data<<std::endl;
-    const float *blob_ptr = (const float *) blob->cpu_data();
+    const float *blob_ptr = (const float *) blob->cpu_data();//get pointer to result
     /*for( int i=0; i<28; i++)
     {
         for(int j=0; j<28; j++)
